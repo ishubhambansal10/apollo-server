0aa0ea72d3c4eb05ab1f843308dab185ad81ab78
ishubhambansal10 <shubham.bansal1@successive.tech>
2021-11-24 01:20:53 +0530

    feat: Subscriptions

--- src/module/trainee/mutation.js
+++ src/module/trainee/mutation.js
@@ -1,16 +1,24 @@
+import pubsub from '../pubsub';
 import userInstance from '../../service/user';
+import constant from '../../lib/constant';
 
 export default {
   createTrainee: (parent, args) => {
     const { user } = args;
-    return userInstance.createUser(user);
+    const addUser = userInstance.createUser(user);
+    pubsub.publish(constant.subscriptions.TRAINEE_ADDED, { traineeAdded: addUser });
+    return addUser;
   },
   updateTrainee: (parent, args) => {
     const { id, role } = args;
-    return userInstance.updateUser(id, role);
+    const updatedUser = userInstance.updateUser(id, role);
+    pubsub.publish(constant.subscriptions.TRAINEE_UPDATED, { traineeUpdated: updatedUser });
+    return updatedUser;
   },
   deleteTrainee: (parent, args) => {
     const { id } = args;
-    return userInstance.deleteUser(id);
+    const deletedId = userInstance.deleteUser(id);
+    pubsub.publish(constant.subscriptions.TRAINEE_DELETED, { traineeDeleted: deletedId });
+    return deletedId;
   },
 };
